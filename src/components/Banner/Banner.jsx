b320aae046edd118dc90ce322ab19aacded63e39
Vishal Bhawani <vishalbhawani999@gmail.com>
2025-04-18 22:00:43 +0530

    work on jsx

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Banner.scss";
 
-
 function Banner() {
     return (
         <section className="banner">
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../../components/Header/Header";
+
 import Banner from "../../components/Banner/Banner";
 import Families from "../../components/Families/Families";
 import Services from "../../components/Services/Services";
@@ -8,7 +8,7 @@ import ServicesLocation from "../../components/ServicesLocation/ServicesLocation
 import WhyUs from "../../components/WhyUs/WhyUs";
 import Faq from "../../components/Faq/Faq";
 import Footer from "../../components/Footer/Footer";
-import CardSlider from "../../components/CardSlider/CardSlider";
+import Header from "../../components/Header/Header";
 const Home = () => {
     return (
         <>
